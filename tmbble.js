0d492b7c4151890552720c542a6e51a686654fe7
Masumi <mutsuda@gmail.com>
2014-03-19 11:00:51 +0100

    Update tmbble.js

--- tmbble.js
+++ tmbble.js
@@ -1,6 +1,6 @@
 ajax({ url: 'http://www.tmb.cat/piu/ca_ES/piuimodesolucio.jsp?parada=0001' }, function(data){
   var headline = data.match(/<title>(.*?)<\/title>/)[1];
-  var body = data.match(/<html>(.*?)<\/html>/)[1];
+  var body = data.match(/<table cellpadding=1 cellspacing=4 align="center">(.*?)<\/table>/)[0];
   simply.title(headline);
   simply.body(body)
 });
