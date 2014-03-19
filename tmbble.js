90f68683fe492589d52340a29f13203ddf2aea87
Masumi Mutsuda Zapater <mutsuda@Hiroshima.local>
2014-03-19 01:05:56 +0100

    Ajax

--- tmbble.js
+++ tmbble.js
@@ -1,22 +1,6 @@
 console.log('Simply.js demo!');
- 
-simply.on('singleClick', function(e) {
-  console.log(util2.format('single clicked $button!', e));
-  simply.subtitle('Has premut ' + e.button + '!');
-});
- 
-simply.on('longClick', function(e) {
-  console.log(util2.format('long clicked $button!', e));
-  simply.vibe();
-  simply.scrollable(e.button !== 'select');
-});
- 
-simply.on('accelTap', function(e) {
-  console.log(util2.format('tapped accel axis $axis $direction!', e));
-  simply.subtitle('Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
-});
- 
-simply.setText({
-  title: 'Simply Demo!',
-  body: 'Això és una demo. Prem un botó!',
-}, true);
+
+ajax({ url: 'http://www.tmb.cat/piu/ca_ES/piuimodesolucio.jsp?parada=0001' }, function(data){
+  var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
+  simply.title(data);
+}); 
