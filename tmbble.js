02f86df5ed6291a241bda8fb2ae0142b98a6dc5f
Masumi Mutsuda Zapater <mutsuda@Hiroshima.local>
2014-03-19 01:13:38 +0100

    Added tmb url

--- tmbble.js
+++ tmbble.js
@@ -1,4 +1,4 @@
-ajax({ url: 'http://simplyjs.io' }, function(data){
-  var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
+ajax({ url: 'http://www.tmb.cat/piu/ca_ES/piuimodesolucio.jsp?parada=0001' }, function(data){
+  var headline = data.match(/<table>(.*?)<\/table>/)[1];
   simply.title(headline);
 });
