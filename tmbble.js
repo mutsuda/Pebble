e159631b3f74aa6e2b0a921a622a3f85dc6099bb
Masumi <mutsuda@gmail.com>
2014-03-19 18:58:16 +0100

    Update tmbble.js

--- tmbble.js
+++ tmbble.js
@@ -2,7 +2,7 @@ var parada = "0001"
 ajax({ url: 'http://www.tmb.cat/piu/ca_ES/piuimodesolucio.jsp?parada='+ parada }, function(data){
   var info = data.match(/<td align="center">(.*?)<\/td>/g);
   simply.title('Parada ' + parada);
-  var output = info[3];
+  var output = "";
   //simply.body(clean(info[3]));
   for (var i = 0; i<info.length; i++)
   {
