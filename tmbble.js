4e1023ca77ef34947680ab461ad28fa840b72fd1
Masumi Mutsuda Zapater <mutsuda@Hiroshima.local>
2014-03-19 01:08:35 +0100

    Deleted line

--- tmbble.js
+++ tmbble.js
@@ -1,6 +1,5 @@
 console.log('Simply.js demo!');
 
 ajax({ url: 'http://www.tmb.cat/piu/ca_ES/piuimodesolucio.jsp?parada=0001' }, function(data){
-  var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
   simply.title(data);
 }); 
