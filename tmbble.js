ed1a46410f3fefa4a70e8ecb84524c0fb5a7c6ce
Masumi Mutsuda Zapater <mutsuda@Hiroshima.local>
2014-03-19 01:11:28 +0100

    Changes

--- tmbble.js
+++ tmbble.js
@@ -1,5 +1,4 @@
-console.log('Simply.js demo!');
-
-ajax({ url: 'http://www.tmb.cat/piu/ca_ES/piuimodesolucio.jsp?parada=0001' }, function(data){
-  simply.title(data);
-}); 
+ajax({ url: 'http://simplyjs.io' }, function(data){
+  var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
+  simply.title(headline);
+});
